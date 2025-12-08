cb03fb57147b448dde300bc09a4ad8cccb1907a6
Jack Langston <github@fullstackjack.io>
2025-12-08 09:09:02 -0500

    feat: kernel hardening
    Signed-off-by: Jack Langston <github@fullstackjack.io>

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,8 +1,6 @@
 {
-  config,
   lib,
   pkgs,
-  modulesPath,
   ...
 }:
 
@@ -46,8 +44,159 @@
     "fs.inotify.max_user_instances" = 1280;
     "fs.inotify.max_user_watches" = 655360;
 
-    # Allow OOM watching
-    "kernel.dmesg_restrict" = 0;
+    # Yama restricts ptrace, which allows processes to read and modify the
+    # memory of other processes. This has obvious security implications.
+    # Default value is 1, to only allow parent processes to ptrace child
+    # processes. May be modified to restrict ptrace further.
+    "kernel.yama.ptrace_scope" = "1";
+
+    # Disables magic sysrq key.
+    "kernel.sysrq" = "0";
+
+    # Disable binfmt. Breaks Roseta, among other applications.
+    # See https://en.wikipedia.org/wiki/Binfmt_misc for more info.
+    "fs.binfmt_misc.status" = "0";
+
+    # Disable io_uring. May be desired for Proxmox, but is responsible
+    # for many vulnerabilities and is disabled on Android + ChromeOS.
+    "kernel.io_uring_disabled" = "2";
+
+    # Disable ip forwarding to reduce attack surface. May be needed for
+    # VM networking.
+    "net.ipv4.ip_forward" = "0";
+    "net.ipv4.conf.all.forwarding" = "0";
+    "net.ipv4.conf.default.forwarding" = "0";
+    "net.ipv6.conf.all.forwarding" = "0";
+    "net.ipv6.conf.default.forwarding" = "0";
+
+    # Privacy/security split.
+    # Enabling tcp-timestamps protects against wrapped sequence numbers
+    # and improves performance, but can leak system time.
+    # URL: (In favor of disabling): https://madaidans-insecurities.github.io/guides/linux-hardening.html#tcp-timestamps
+    # URL: (In favor of enabling): https://access.redhat.com/sites/default/files/attachments/20150325_network_performance_tuning.pdf
+    "net.ipv4.tcp_timestamps" = "1";
+
+    "dev.tty.ldisc_autoload" = "0";
+    "fs.protected_fifos" = "2";
+    "fs.protected_hardlinks" = "1";
+    "fs.protected_regular" = "2";
+    "fs.protected_symlinks" = "1";
+    "fs.suid_dumpable" = "0";
+    "kernel.dmesg_restrict" = "1";
+    "kernel.kexec_load_disabled" = "1";
+    "kernel.kptr_restrict" = "2";
+    "kernel.perf_event_paranoid" = "3";
+    "kernel.printk" = "3 3 3 3";
+    "kernel.unprivileged_bpf_disabled" = "1";
+    "net.core.bpf_jit_harden" = "2";
+
+    # Disable ICMP redirects to prevent some MITM attacks
+    # See https://askubuntu.com/questions/118273/what-are-icmp-redirects-and-should-they-be-blocked
+    "net.ipv4.conf.all.accept_redirects" = "0";
+    "net.ipv4.conf.default.accept_redirects" = "0";
+    "net.ipv4.conf.all.send_redirects" = "0";
+    "net.ipv4.conf.default.send_redirects" = "0";
+    "net.ipv6.conf.all.accept_redirects" = "0";
+    "net.ipv6.conf.default.accept_redirects" = "0";
+
+    # Use secure ICMP redirects by default. Helpful if ICMP redirects are
+    # reenabled only.
+    "net.ipv4.conf.all.secure_redirects" = "1";
+    "net.ipv4.conf.default.secure_redirects" = "1";
+
+    "net.ipv4.conf.all.accept_source_route" = "0";
+    "net.ipv4.conf.all.rp_filter" = "1";
+    "net.ipv4.conf.default.accept_source_route" = "0";
+    "net.ipv4.conf.default.rp_filter" = "1";
+    "net.ipv4.icmp_echo_ignore_all" = "1";
+    "net.ipv6.icmp_echo_ignore_all" = "1";
+    "net.ipv4.tcp_dsack" = "0";
+    "net.ipv4.tcp_fack" = "0";
+    "net.ipv4.tcp_rfc1337" = "1";
+    "net.ipv4.tcp_sack" = "0";
+    "net.ipv4.tcp_syncookies" = "1";
+    "net.ipv6.conf.all.accept_ra" = "0";
+    "net.ipv6.conf.all.accept_source_route" = "0";
+    "net.ipv6.conf.default.accept_source_route" = "0";
+    "net.ipv6.default.accept_ra" = "0";
+    "kernel.core_pattern" = "|/bin/false";
+    "vm.mmap_rnd_bits" = "32";
+    "vm.mmap_rnd_compat_bits" = "16";
+    "vm.unprivileged_userfaultfd" = "0";
+    "net.ipv4.icmp_ignore_bogus_error_responses" = "1";
+
+    # Enable ASLR
+    # Turn on protection and randomize stack, vdso page and mmap + randomize brk base address
+    "kernel.randomize_va_space" = "2";
+
+    # Restrict perf subsystem usage (activity) further
+    "kernel.perf_cpu_time_max_percent" = "1";
+    "kernel.perf_event_max_sample_rate" = "1";
+
+    # Do not allow mmap in lower addresses
+    "vm.mmap_min_addr" = "65536";
+
+    # Log packets with impossible addresses to kernel log
+    # No active security benefit, just makes it easier to spot a DDOS/DOS by giving
+    # extra logs
+    "net.ipv4.conf.default.log_martians" = "1";
+    "net.ipv4.conf.all.log_martians" = "1";
+
+    # Disable sending and receiving of shared media redirects
+    # This setting overwrites net.ipv4.conf.all.secure_redirects
+    # Refer to RFC1620
+    "net.ipv4.conf.default.shared_media" = "0";
+    "net.ipv4.conf.all.shared_media" = "0";
+
+    # Always use the best local address for announcing local IP via ARP
+    # Seems to be most restrictive option
+    "net.ipv4.conf.default.arp_announce" = "2";
+    "net.ipv4.conf.all.arp_announce" = "2";
+
+    # Reply only if the target IP address is local address configured on the incoming interface
+    "net.ipv4.conf.default.arp_ignore" = "1";
+    "net.ipv4.conf.all.arp_ignore" = "1";
+
+    # Drop Gratuitous ARP frames to prevent ARP poisoning
+    # This can cause issues when ARP proxies are used in the network
+    "net.ipv4.conf.default.drop_gratuitous_arp" = "1";
+    "net.ipv4.conf.all.drop_gratuitous_arp" = "1";
+
+    # Ignore all ICMP echo and timestamp requests sent to broadcast/multicast
+    "net.ipv4.icmp_echo_ignore_broadcasts" = "1";
+
+    # Number of Router Solicitations to send until assuming no routers are present
+    "net.ipv6.conf.default.router_solicitations" = "0";
+    "net.ipv6.conf.all.router_solicitations" = "0";
+
+    # Do not accept Router Preference from RA
+    "net.ipv6.conf.default.accept_ra_rtr_pref" = "0";
+    "net.ipv6.conf.all.accept_ra_rtr_pref" = "0";
+
+    # Learn prefix information in router advertisement
+    "net.ipv6.conf.default.accept_ra_pinfo" = "0";
+    "net.ipv6.conf.all.accept_ra_pinfo" = "0";
+
+    # Setting controls whether the system will accept Hop Limit settings from a router advertisement
+    "net.ipv6.conf.default.accept_ra_defrtr" = "0";
+    "net.ipv6.conf.all.accept_ra_defrtr" = "0";
+
+    # Router advertisements can cause the system to assign a global unicast address to an interface
+    "net.ipv6.conf.default.autoconf" = "0";
+    "net.ipv6.conf.all.autoconf" = "0";
+
+    # Number of neighbor solicitations to send out per address
+    "net.ipv6.conf.default.dad_transmits" = "0";
+    "net.ipv6.conf.all.dad_transmits" = "0";
+
+    # Number of global unicast IPv6 addresses can be assigned to each interface
+    "net.ipv6.conf.default.max_addresses" = "1";
+    "net.ipv6.conf.all.max_addresses" = "1";
+
+    # Ignore all ICMPv6 echo requests
+    "net.ipv6.icmp.echo_ignore_all" = "1";
+    "net.ipv6.icmp.echo_ignore_anycast" = "1";
+    "net.ipv6.icmp.echo_ignore_multicast" = "1";
   };
 
   # Don't store coredumps from systemd-coredump.
@@ -109,6 +258,13 @@
     "intel_iommu=on" # Enable Intel IOMMU for DMA remapping
     "iommu=force" # Force enable IOMMU even on platforms that normally disable it
     "iommu.strict=1" # Use strict mode - TLB flush on each unmap (more secure, slight performance cost)
+    "iommu.passthrough=0" # Forces DMA to go through IOMMU to mitigate some DMA attacks
+
+    # Additional security mitigations
+    "efi=disable_early_pci_dma" # May prevent some systems from booting
+    "mitigations=auto,nosmt" # Apply relevant CPU exploit mitigations, and disable symmetric multithreading. May harm performance.
+    "pti=on" # Mitigates Meltdown, some KASLR bypasses. Hurts performance.
+    "extra_latent_entropy" # Gather more entropy on boot. Only works with the linux_hardened patchset, but does nothing if using another kernel. Slows down boot time by a bit.
   ];
 
   environment.systemPackages = with pkgs; [
